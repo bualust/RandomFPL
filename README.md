2c4314fe71ebf3ac59953bca8791c8213400aa13
Valentina Vecchio <valentina.vecchio12@gmail.com>
2022-08-25 16:11:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RandomFPL
+As everybody around me only talks about PL and FPL I decide to join with a random generated team.
+This code uses the FPL API [https://fpl.readthedocs.io/en/latest/] to generate a team removing all the players who are injured or suspended and mazimising the total amount of money spent.
+
+To run
+`python fantasy_random.py`
---
+++ fantasy_random.py
@@ -0,0 +1,127 @@
+import asyncio
+import aiohttp
+from prettytable import PrettyTable
+from fpl import FPL
+import pandas as pd
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        fpl = FPL(session)
+        players = await fpl.get_players()
+
+    df = pandas_df_players(players)
+    df = select_only_active(df)
+    random_team = get_random_team(df, True, df)
+    random_team = maximise_expense(random_team, df)
+    print_pretty_table(random_team)
+    print('Congratulations! You spent ', sum(random_team['Price']))
+
+def select_only_active(df):
+    df = df[df['Status']=='a']
+    return df
+
+def pandas_df_players(players):
+
+    names = []
+    price = []
+    position = []
+    news = []
+    status = []
+    for player in players:
+        names.append(player.web_name)
+        price.append(float(player.now_cost / 10))
+        position.append(int(player.element_type))
+        news.append((player.news))
+        status.append((player.status))
+
+    df = pd.DataFrame(
+        {
+            "Name" : names,
+            "Price" : price,
+            "Position": position,
+            "News": news,
+            "Status": status,
+        }
+    )
+    return df
+
+def get_random_team(df, isFirstAttempt, random_team):
+
+    goal_keepers = df[df["Position"]==1]
+    defenders    = df[df["Position"]==2]
+    midfielders  = df[df["Position"]==3]
+    forwards     = df[df["Position"]==4]
+
+    if isFirstAttempt==True:
+        print('Generating first random team')
+        random_GKPs = goal_keepers.sample(2)
+        random_DEF  = defenders.sample(5)
+        random_MID  = midfielders.sample(5)
+        random_FWD  = forwards.sample(3)
+        random_team = pd.concat([random_GKPs, random_DEF, random_MID, random_FWD])
+    else:
+        removed_player, new_member = get_new_player(random_team, df)
+        print('Please say goodbye to --- ')
+        print(removed_player)
+        player_loc  = removed_player.head().index.values
+        random_team = random_team.drop(index=player_loc, axis=1)
+        print('--- and welcome out new team member')
+        print(new_member)
+        random_team = pd.concat([random_team, new_member])
+
+    return random_team
+
+def get_new_player(random_team, df):
+
+    pick_one_player  = random_team.sample(1)
+    removed_position = int(pick_one_player.iloc[0]['Position'])
+    removed_price    = float(pick_one_player.iloc[0]['Price'])
+    new_candidates   = df[df["Position"]==removed_position]
+    new_candidates   = new_candidates[new_candidates['Price']!=removed_price]
+    new_candidates   = new_candidates[new_candidates['Price']>removed_price]
+
+    while new_candidates.empty:
+        pick_one_player  = random_team.sample(1)
+        removed_position = int(pick_one_player.iloc[0]['Position'])
+        removed_price    = float(pick_one_player.iloc[0]['Price'])
+        new_candidates   = df[df["Position"]==removed_position]
+        new_candidates   = new_candidates[new_candidates['Price']!=removed_price]
+        new_candidates   = new_candidates[new_candidates['Price']>removed_price]
+
+    new_member       = new_candidates.sample(1)
+
+
+    return pick_one_player, new_member
+
+def maximise_expense(random_team, df):
+
+    expense = sum(random_team['Price'])
+    iterations = 0
+    while expense < 100.0:
+        if iterations > 50: break
+        iterations += 1
+        print('We have money to use, lets generate another team')
+        new_random_team = get_random_team(df, False, random_team)
+        new_expense = sum(new_random_team['Price'])
+        if new_expense > expense and new_expense < 100.0:
+            random_team = new_random_team
+            expense = new_expense
+        else:
+            print('Confirming team for total expense of == ',expense)
+
+    return random_team
+
+
+def print_pretty_table(random_team):
+    player_table = PrettyTable()
+    player_table.field_names = ["Player", "Price", "Element Type",
+                                "News", "Status"]
+    player_table.align["Player"] = "l"
+    for index, player in random_team.iterrows():
+        player_table.add_row([player['Name'], player['Price'],
+                            player['Position'],player['News'], player['Status']])
+
+    print(player_table)
+
+if __name__ == "__main__":
+    asyncio.run(main())
