1d85717620aef05a184b85412e15c33f005fd032
Valentina Vecchio <valentina.vecchio12@gmail.com>
2022-12-30 09:27:22 +0000

    commented functions, plots and more

--- randomfpl/fantasy_random.py
+++ randomfpl/fantasy_random.py
@@ -7,21 +7,25 @@ import pandas as pd
 
 def main():
     """`main` function for RandomFPL module"""
-    #asyncio.run(generate_team())
-    generate_team()
+    asyncio.run(generate_team())
+    #generate_team()
 
-#Read PL players from website and gives you a random team
-#async def generate_team():
-def generate_team():
+async def generate_team():
+#def generate_team():
+    """`generate_team` Read PL players from website and gives you a random team"""
 
-    #async with aiohttp.ClientSession() as session:
-    #    fpl = FPL(session)
-    #    players = await fpl.get_players()
+    async with aiohttp.ClientSession() as session:
+        fpl = FPL(session)
+        players = await fpl.get_players()
 
 
-    #df = pandas_df_players(players)
-    df = pd.read_csv('Output.csv')
+    df = pandas_df_players(players)
+    #df = pd.read_csv('Output.csv')
     df = select_only_active(df)
+    max_minutes = max(df["Minutes"])
+    print("All active players", len(df))
+    df = df[df["Minutes"]>max_minutes*0.5]
+    print("All active players that played more than max/2", len(df))
     #df.to_csv('Output.csv',index=False)
 
     av_g_f,av_a_m,av_g_c = print_average_quantities(df)
@@ -30,43 +34,45 @@ def generate_team():
     print_pretty_table(random_team)
 
     print('Congratulations! You spent ', sum(random_team['Price']))
-    print("==========================")
-    print("Average Goals Forwards", av_g_f)
-    print("Average Assists Midfielders", av_a_m)
-    print("Average Goals conceded defenders", av_g_c)
-    print("==========================")
 
     to_plot = {'GP90':'Number of goals scored per 90mins played',
                'AP90':'Number of assists per 90mins played',
                'GCP90':'Number of goals conceded per 90mins played'}
     for plot,title in to_plot.items():
         fig, ax = plt.subplots()
-        max_val = int(max(df[plot]))
-        min_val = int(min(df[plot]))
+        max_val = float(max(df[plot]))
+        min_val = float(min(df[plot]))
         number_of_bins = 10
+        if plot=='GCP90': number_of_bins = 20
         edges = (max_val-min_val)/number_of_bins
         bins = []
         i = 0
-        while i<number_of_bins:
+        while i<=number_of_bins:
             bins.append(min_val+i*edges)
             i=i+1
-        plt.hist(df[plot], histtype='step', label="All players", density=True, bins=bins)
+        #plt.hist(df[plot], histtype='step', label="All players", density=True, bins=bins)
         forwards = df[df['Position']==4]
-        plt.hist(forwards[plot], histtype='step', label="Forwards", density=True, bins=bins)
+        if plot!='GCP90': plt.hist(forwards[plot], histtype='step', label="Forwards", density=True, bins=bins)
         midfields = df[df['Position']==3]
-        plt.hist(midfields[plot], histtype='step', label="Midfielders", density=True, bins=bins)
-        plt.hist(random_team[plot], histtype='step', label="Random Penguins", density=True, bins=bins)
+        if plot!='GCP90': plt.hist(midfields[plot], histtype='step', label="Midfielders", density=True, bins=bins)
+        defenders = df[df['Position']==2]
+        if plot=='GCP90': plt.hist(defenders[plot], histtype='step', label="Defenders", density=True, bins=bins)
+        plt.hist(random_team[plot], histtype='step', label="Random Penguins", density=False, bins=bins)
         ax.legend()
         plt.xlabel(to_plot[plot])
         plt.savefig(plot+'.png')
+    df_maxgoals   = df.loc[df['GP90'].idxmax()]
+    df_maxassists = df.loc[df['AP90'].idxmax()]
+    df_mingoalscon= df.loc[df['GCP90'].idxmin()]
+    print('Max number of goals\n',df_maxgoals,
+          "\nMax number of assists\n", df_maxassists,
+          "\nMin number of goals conceded\n", df_mingoalscon)
 
 def print_average_quantities(df):
+    """`print_average_quantities` prints average goals and assists"""
     forwards    = df[df['Position']==4]
     midfielders = df[df['Position']==3]
     defender    = df[df['Position']==2]
-    forwards    = forwards[forwards["Minutes"]!=0]
-    midfielders = midfielders[midfielders["Minutes"]!=0]
-    defender    = defender[defender["Minutes"]!=0]
     av_g_f      = sum(forwards["GP90"])/len(forwards)
     av_g_m      = sum(midfielders["GP90"])/len(midfielders)
     av_a_f      = sum(forwards["AP90"])/len(forwards)
@@ -83,14 +89,14 @@ def print_average_quantities(df):
 
     return av_g_f,av_a_m,av_g_c
 
-#removes injured and suspended players
 def select_only_active(df):
+    """`select_only_active` removes injured and suspended players"""
     df = df[df['Status']=='a']
     df = df[df['Minutes']!=0]
     return df
 
-#creates pandas dataframe of players
 def pandas_df_players(players):
+    """`pandas_df_players` creates pandas dataframe of players"""
 
     names = []
     price = []
@@ -142,8 +148,8 @@ def pandas_df_players(players):
 
     return df
 
-#generates a random team from pandasDF
 def get_random_team(df, isFirstAttempt, random_team, av_g_f, av_a_m, av_g_c):
+    """`get_random_team` generates a random team from pandasDF"""
 
     goal_keepers = df[df["Position"]==1]
     defenders    = df[df["Position"]==2]
@@ -151,7 +157,6 @@ def get_random_team(df, isFirstAttempt, random_team, av_g_f, av_a_m, av_g_c):
     forwards     = df[df["Position"]==4]
 
     if isFirstAttempt==True:
-        print('Generating first random team')
         random_GKPs = goal_keepers.sample(2)
         random_DEF  = defenders.sample(5)
         random_MID  = midfielders.sample(5)
@@ -160,41 +165,46 @@ def get_random_team(df, isFirstAttempt, random_team, av_g_f, av_a_m, av_g_c):
     else:
         removed_player, new_member = substituion(random_team, df,
                                                  av_g_f, av_a_m, av_g_c)
-        print('Please say goodbye to --- ')
-        print(removed_player)
         player_loc  = removed_player.head().index.values
         random_team = random_team.drop(index=player_loc, axis=1)
-        print('--- and welcome our new team member')
-        print(new_member)
         random_team = pd.concat([random_team, new_member])
 
     return random_team
 
-#removes one player randomly from team and substitues
-#with a player of same position but more expensive
 def select_new_candidates(random_team, df, av_g_f,av_a_m,av_g_c):
+    """`select_new_candidates`removes one player randomly from team and substitues"""
+    """with a player of same position but more expensive"""
 
     extracted_player  = random_team.sample(1)
     removed_position = int(extracted_player.iloc[0]['Position'])
     removed_price    = float(extracted_player.iloc[0]['Price'])
+    removed_gp90     = float(extracted_player.iloc[0]['GP90'])
+    removed_ap90     = float(extracted_player.iloc[0]['AP90'])
+    removed_gcp90     = float(extracted_player.iloc[0]['GCP90'])
     new_candidates   = df[df["Position"]==removed_position]
     new_candidates   = new_candidates[new_candidates['Price']!=removed_price]
     new_candidates   = new_candidates[new_candidates['Price']>removed_price]
     if removed_position==4:
+        new_candidates   = new_candidates[new_candidates['GP90']>removed_gp90]
         new_candidates   = new_candidates[new_candidates['GP90']>av_g_f]
+        new_candidates   = new_candidates[new_candidates['AP90']>removed_ap90]
         new_candidates   = new_candidates[new_candidates['AP90']>av_a_m]
     elif removed_position==3:
-        new_candidates   = new_candidates[new_candidates['AP90']>av_a_m]
+        new_candidates   = new_candidates[new_candidates['GCP90']<removed_gcp90]
         new_candidates   = new_candidates[new_candidates['GCP90']<av_g_c]
+        new_candidates   = new_candidates[new_candidates['AP90']>removed_ap90]
+        new_candidates   = new_candidates[new_candidates['AP90']>av_a_m]
     elif removed_position==2:
-        new_candidates   = new_candidates[new_candidates['AP90']>av_a_m]
+        new_candidates   = new_candidates[new_candidates['GCP90']<removed_gcp90]
         new_candidates   = new_candidates[new_candidates['GCP90']<av_g_c]
+        new_candidates   = new_candidates[new_candidates['AP90']>removed_ap90]
+        new_candidates   = new_candidates[new_candidates['AP90']>av_a_m]
 
     return extracted_player, new_candidates
 
-#there might be sometimes no candidate to make substitution
-#select a new player to remove
 def substituion(random_team, df, av_g_f,av_a_m,av_g_c):
+    """`substituion` there might be sometimes no candidate to make substitution"""
+    """select a new player to remove"""
 
     extracted_player, new_candidates = select_new_candidates(random_team, df,
                                                              av_g_f,av_a_m,av_g_c)
@@ -206,9 +216,9 @@ def substituion(random_team, df, av_g_f,av_a_m,av_g_c):
 
     return extracted_player, new_member
 
-#keeps removing one player and replacing it until
-#all money are spent (cap up to 50 iterations)
 def maximise_expense(random_team, df, av_g_f,av_a_m,av_g_c):
+    """`maximise_expense` keeps removing one player and replacing it until"""
+    """all money are spent (cap up to 50 iterations)"""
 
     expense = sum(random_team['Price'])
     iterations = 0
@@ -216,25 +226,37 @@ def maximise_expense(random_team, df, av_g_f,av_a_m,av_g_c):
     while expense < 102.1:
         if iterations > 50: break
         iterations += 1
-        print('We have money to use, lets generate another team')
         new_random_team = get_random_team(df, False, random_team, av_g_f,av_a_m,av_g_c)
         new_expense = sum(new_random_team['Price'])
-        duplicate = new_random_team[new_random_team.duplicated()]
-        while duplicate.empty==False:
+        duplicate, same_team_players, i = find_duplicates(new_random_team)
+        while duplicate.empty==False or i>=3:
             new_random_team = get_random_team(df, False, random_team, av_g_f,av_a_m,av_g_c)
             new_expense = sum(new_random_team['Price'])
-            duplicate = new_random_team[new_random_team.duplicated()]
+            duplicate, same_team_players, i = find_duplicates(new_random_team)
         if new_expense > expense and new_expense < 102.1:
             random_team = new_random_team
             expense = new_expense
-        else:
-            print('Confirming team for total expense of == ',expense)
+
+    print('Confirming team for total expense of == ',expense)
 
     return random_team
 
+def find_duplicates(team_DF):
+    """`find_duplicates` finds player if added twice to the team"""
+    """finds players and same team and number of"""
+    """players in the same team-1"""
 
-#makes a pretty table of your random team
+    duplicate = team_DF[team_DF.duplicated()]
+    same_team_players = team_DF[team_DF.duplicated(subset='Team')]
+    i = 0
+    while same_team_players.empty==False:
+        same_team_players = same_team_players[same_team_players.duplicated(subset='Team')]
+        i+=1
+    return duplicate, same_team_players, i
+
+
 def print_pretty_table(random_team):
+    """`print_pretty_table` makes a pretty table of your random team"""
     player_table = PrettyTable()
     player_table.field_names = ["Player", "Price", "Element Type",
                                 "Team", "Status", "Minutes", "Goals",
